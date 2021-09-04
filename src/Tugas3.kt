946cb6ea2df7f3cf30311ef6603087203e318df7
adniasalsabila <adniasalsabila8@gmail.com>
2021-09-04 17:00:50 +0700

    Initial Commit

--- src/Tugas3.kt
+++ src/Tugas3.kt
@@ -1,9 +1,25 @@
 fun main() {
-    val i: Int = 2
-    for (i in 1..10) {
-        if (i == 5) {
-            break
+
+    var angka: Int
+    println("Perulangan Bilangan Ganjil dan Genap")
+    print("Masukkan angka = ")
+    angka = readLine()!!.toInt()
+    if (angka % 2 == 0) {
+        print("Angka $angka termasuk Bilangan Genap")
+        for (i in 1..10) {
+            if (i == 5) {
+                break
+            }
+            println(i)
         }
-        println(i)
+    } else {
+        print("Angka $angka termasuk Bilangan Ganjilap")
+        print("Angka $angka termasuk Bilangan Genap")
+        for (i in 1..10) {
+            if (i == 5) {
+                continue
+            }
+            println(i)
+        }
     }
 }
